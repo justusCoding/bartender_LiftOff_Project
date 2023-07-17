a80a419250662925769abde3fc30dde0651d9ee6
Andrew Deason <deasonam@gmail.com>
2023-07-17 15:54:41 -0500

    implementing (very) basic search functionality

--- src/main/java/org/launchcode/bartender_LiftOff_Project/cocktails/data/CocktailRepository.java
+++ src/main/java/org/launchcode/bartender_LiftOff_Project/cocktails/data/CocktailRepository.java
@@ -4,6 +4,9 @@ import org.launchcode.bartender_LiftOff_Project.cocktails.models.Cocktail;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CocktailRepository extends CrudRepository<Cocktail, Integer> {
+    List<Cocktail> findByNameContainingIgnoreCase(String name);
 }
