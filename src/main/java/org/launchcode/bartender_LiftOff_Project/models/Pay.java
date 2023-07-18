c20601e71cfa7b83eb9fb5127226afbbddbee09a
katherineernsting <katherine.e.kennison@gmail.com>
2023-07-17 20:20:38 -0500

    Adding validation to Pay model WIP

--- src/main/java/org/launchcode/bartender_LiftOff_Project/models/Pay.java
+++ src/main/java/org/launchcode/bartender_LiftOff_Project/models/Pay.java
@@ -1,17 +1,46 @@
 package org.launchcode.bartender_LiftOff_Project.models;
 
-        public class Pay {
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Pay {
 
             private final int id;
             private int nextId = 1;
 
+            @NotBlank
             private String dateWorked;
+
+            @NotBlank
+            @DecimalMin(value = "0.25")
+            @DecimalMax(value = "24")
             private double hoursWorked;
+
+            @NotBlank
             private String shiftWorked;
+
+            @NotBlank
+            @DecimalMin(value = "0.25")
+            @DecimalMax(value = "1000000")
             private double hourlyRate;
+
+            @NotBlank
+            @DecimalMin(value = "0.25")
+            @DecimalMax(value = "1000000")
             private double creditCardTips;
+
+            @NotBlank
+            @DecimalMin(value = "0.25")
+            @DecimalMax(value = "1000000")
             private double cashTips;
+
+            @NotBlank
+            @Size(min = 0, max = 100)
             private double taxRate;
+//            TODO: Make taxRate an enum?
+
             private double totalPay;
 
             public Pay(String dateWorked, double hoursWorked, String shiftWorked,
