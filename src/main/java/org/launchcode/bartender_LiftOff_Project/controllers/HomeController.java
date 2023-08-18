682d114d09a2cd0242c5ebe1ad1ef7ca7365bdd7
Andrew Deason <deasonam@gmail.com>
2023-08-17 19:22:28 -0500

    show/hide login/logout link

--- src/main/java/org/launchcode/bartender_LiftOff_Project/controllers/HomeController.java
+++ src/main/java/org/launchcode/bartender_LiftOff_Project/controllers/HomeController.java
@@ -1,17 +1,27 @@
 package org.launchcode.bartender_LiftOff_Project.controllers;
 
+import org.launchcode.bartender_LiftOff_Project.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+    @Autowired
+    AuthenticationController authenticationController;
     @GetMapping
-    public String index(Model model) {
+    public String index(Model model, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
         model.addAttribute("title", "Mix Muse");
+        model.addAttribute("user", user);
 
         return "index"; }
 
